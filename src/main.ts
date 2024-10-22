cda70e4625a83cac2cdb4762f639ad827356be35
nupur <nupur@alt-mobilty.com>
2024-10-22 23:27:07 +0530

    swagger add

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Fitness Tracker API')
+    .setDescription('API documentation for the Fitness Tracker application')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document); 
+
+  await app.listen(3000);
 }
 bootstrap();
